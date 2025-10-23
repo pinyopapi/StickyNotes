59cd11347724f608cc8e5bcd7d2944c460220abe
pinyopapi <95istvan@gmail.com>
2025-10-23 11:10:15 +0200

    Add note repository

---
+++ src/StickyNotes.Infrastucture/Repositories/NoteRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StickyNotes.Domain.Entities;
+using StickyNotes.Application.Interfaces;
+using StickyNotes.Infrastructure.Persistence;
+
+namespace StickyNotes.Infrastructure.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly AppDbContext _context;
+
+        public NoteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Note note)
+        {
+            await _context.Notes.AddAsync(note);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var note = await _context.Notes.FirstOrDefaultAsync(n => n.Id == id);
+            if (note != null)
+            {
+                _context.Notes.Remove(note);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Note>> GetAllAsync(Guid? userId = null)
+        {
+            var query = _context.Notes.AsQueryable();
+            if (userId.HasValue)
+                query = query.Where(n => n.UserId == userId.Value);
+            return await query.ToListAsync();
+        }
+
+        public async Task<Note> GetByIdAsync(Guid id)
+        {
+            return await _context.Notes.FirstOrDefaultAsync(n => n.Id == id);
+        }
+
+        public async Task UpdateAsync(Note note)
+        {
+            _context.Notes.Update(note);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
