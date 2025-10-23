ec9b752dda8f5d0d37cd0146b4f81fb09c5d1938
pinyopapi <95istvan@gmail.com>
2025-10-23 10:48:37 +0200

    Add db context

---
+++ src/StickyNotes.Infrastucture/Persistence/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StickyNotes.Domain.Entities;
+
+namespace StickyNotes.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
