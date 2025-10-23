a09f7b793821a6516a68888e7a58892cda52d3b7
pinyopapi <95istvan@gmail.com>
2025-10-23 09:44:21 +0200

    Add note repository interface

---
+++ src/StickyNotes.Application/Interfaces/INoteRepository.cs
@@ -0,0 +1,24 @@
+﻿using StickyNotes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StickyNotes.Application.Interfaces
+{
+    public interface INoteRepository
+    {
+        Task<IEnumerable<Note>> GetAllNotesAsync(Guid? userId = null);
+        Task<Note> GetNoteByIdAsync(Guid id);
+        Task<Note> CreateNoteAsync(string title, string content, Guid userId);
+        Task<Note> UpdateNoteAsync(Guid id, string title, string content);
+        Task DeleteNoteAsync(Guid id);
+        Task PinNoteAsync(Guid id);
+        Task UnpinNoteAsync(Guid id);
+        Task ArchiveNoteAsync(Guid id);
+        Task RestoreNoteAsync(Guid id);
+        Task AddTagAsync(Guid id, string tag);
+        Task RemoveTagAsync(Guid id, string tag);
+        Task ChangeColorAsync(Guid id, string color);
+        Task SetPositionAsync(Guid id, float x, float y);
+    }
+}
