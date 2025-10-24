612e6aeeaf24d729b1e4ab2b1cada6ca6b82e54c
pinyopapi <95istvan@gmail.com>
2025-10-24 11:29:36 +0200

    Add note creation test

---
+++ src/StickyNotes.Tests/StickyNotes.Tests/ApplicationTests/NoteServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using StickyNotes.Application.Services;
+
+namespace StickyNotes.Tests.ApplicationTests;
+
+[TestFixture]
+public class NoteServiceTests
+{
+    private NoteService _service;
+    private FakeNoteRepository _repository;
+    private Guid _userId;
+
+    [SetUp]
+    public void Setup()
+    {
+        _repository = new FakeNoteRepository();
+        _service = new NoteService(_repository);
+        _userId = Guid.NewGuid();
+    }
+
+    [Test]
+    public async Task CreateNote_Should_AddNote()
+    {
+        var note = await _service.CreateNoteAsync("Test", "Content", _userId);
+
+        var allNotes = await _repository.GetAllAsync(_userId);
+        Assert.That(allNotes.Count(), Is.EqualTo(1));
+        Assert.That(note.Title, Is.EqualTo("Test"));
+    }
+
+}
