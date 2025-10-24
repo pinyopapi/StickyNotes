2d45afd7508bf28c0ec295825408a39956c4d190
pinyopapi <95istvan@gmail.com>
2025-10-24 10:46:55 +0200

    Add exception handling

--- src/StickyNotes.Application/Services/Noteservice.cs
+++ src/StickyNotes.Application/Services/Noteservice.cs
@@ -12,7 +12,7 @@ namespace StickyNotes.Application.Services
 
         public NoteService(INoteRepository repository)
         {
-            _repository = repository;
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
         }
 
         public async Task<IEnumerable<Note>> GetAllNotesAsync(Guid? userId = null)
@@ -22,13 +22,16 @@ namespace StickyNotes.Application.Services
 
         public async Task<Note> GetNoteByIdAsync(Guid id)
         {
-            var note = await _repository.GetByIdAsync(id);
-            if (note == null) throw new KeyNotFoundException("Note not found");
+            var note = await _repository.GetByIdAsync(id)
+                       ?? throw new KeyNotFoundException($"Note with id {id} not found.");
             return note;
         }
 
         public async Task<Note> CreateNoteAsync(string title, string content, Guid userId)
         {
+            if (userId == Guid.Empty)
+                throw new ArgumentException("Invalid user ID.");
+
             var note = new Note(title, content, userId);
             await _repository.AddAsync(note);
             return note;
@@ -45,63 +48,17 @@ namespace StickyNotes.Application.Services
         public async Task DeleteNoteAsync(Guid id)
         {
             var note = await GetNoteByIdAsync(id);
-            await _repository.DeleteAsync(id);
+            await _repository.DeleteAsync(note.Id);
         }
 
-        public async Task PinNoteAsync(Guid id)
-        {
-            var note = await GetNoteByIdAsync(id);
-            note.Pin();
-            await _repository.UpdateAsync(note);
-        }
+        public async Task PinNoteAsync(Guid id) => await ApplyDomainAction(id, n => n.Pin());
+        public async Task UnpinNoteAsync(Guid id) => await ApplyDomainAction(id, n => n.Unpin());
+        public async Task ArchiveNoteAsync(Guid id) => await ApplyDomainAction(id, n => n.Archive());
+        public async Task RestoreNoteAsync(Guid id) => await ApplyDomainAction(id, n => n.Restore());
+        public async Task AddTagAsync(Guid id, string tag) => await ApplyDomainAction(id, n => n.AddTag(tag));
+        public async Task RemoveTagAsync(Guid id, string tag) => await ApplyDomainAction(id, n => n.RemoveTag(tag));
+        public async Task ChangeColorAsync(Guid id, string color) => await ApplyDomainAction(id, n => n.ChangeColor(color));
+        public async Task SetPositionAsync(Guid id, float x, float y) => await ApplyDomainAction(id, n => n.SetPosition(x, y));
 
-        public async Task UnpinNoteAsync(Guid id)
-        {
-            var note = await GetNoteByIdAsync(id);
-            note.Unpin();
-            await _repository.UpdateAsync(note);
-        }
-
-        public async Task ArchiveNoteAsync(Guid id)
-        {
-            var note = await GetNoteByIdAsync(id);
-            note.Archive();
-            await _repository.UpdateAsync(note);
-        }
-
-        public async Task RestoreNoteAsync(Guid id)
-        {
-            var note = await GetNoteByIdAsync(id);
-            note.Restore();
-            await _repository.UpdateAsync(note);
-        }
-
-        public async Task AddTagAsync(Guid id, string tag)
-        {
-            var note = await GetNoteByIdAsync(id);
-            note.AddTag(tag);
-            await _repository.UpdateAsync(note);
-        }
-
-        public async Task RemoveTagAsync(Guid id, string tag)
-        {
-            var note = await GetNoteByIdAsync(id);
-            note.RemoveTag(tag);
-            await _repository.UpdateAsync(note);
-        }
-
-        public async Task ChangeColorAsync(Guid id, string color)
-        {
-            var note = await GetNoteByIdAsync(id);
-            note.ChangeColor(color);
-            await _repository.UpdateAsync(note);
-        }
-
-        public async Task SetPositionAsync(Guid id, float x, float y)
-        {
-            var note = await GetNoteByIdAsync(id);
-            note.SetPosition(x, y);
-            await _repository.UpdateAsync(note);
-        }
     }
 }
