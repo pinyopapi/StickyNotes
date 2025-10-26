5bf3754f7c27b74e19a05e4f75f6b8e73fe4bb39
pinyopapi <95istvan@gmail.com>
2025-10-26 11:19:02 +0100

    Define note service points

---
+++ frontend/src/services/noteService.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const API_URL = 'https://localhost:5001/api/note';
+
+export const getAllNotes = (userId) => {
+    return axios.get(API_URL, { params: { userId } });
+};
+
+export const getNoteById = (id) => {
+    return axios.get(`${API_URL}/${id}`);
+};
+
+export const createNote = (title, content, userId) => {
+    return axios.post(API_URL, { title, content, userId });
+};
+
+export const updateNote = (id, title, content) => {
+    return axios.put(`${API_URL}/${id}`, { title, content });
+};
+
+export const deleteNote = (id) => {
+    return axios.delete(`${API_URL}/${id}`);
+};
+
+export const pinNote = (id) => axios.put(`${API_URL}/${id}/pin`);
+export const unpinNote = (id) => axios.put(`${API_URL}/${id}/unpin`);
+export const archiveNote = (id) => axios.put(`${API_URL}/${id}/archive`);
+export const restoreNote = (id) => axios.put(`${API_URL}/${id}/restore`);
+export const changeColor = (id, color) => axios.put(`${API_URL}/${id}/color`, { color });
+export const setPosition = (id, x, y) => axios.put(`${API_URL}/${id}/position`, { x, y });
+export const addTag = (id, tag) => axios.put(`${API_URL}/${id}/tags/add`, { tag });
+export const removeTag = (id, tag) => axios.put(`${API_URL}/${id}/tags/remove`, { tag });
