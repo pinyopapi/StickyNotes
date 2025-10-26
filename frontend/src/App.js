277fb5553693a34dc983b54af88f982f0aff5800
pinyopapi <95istvan@gmail.com>
2025-10-26 12:10:44 +0100

    Add base page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
+import React, { useState } from 'react';
+import NoteList from './components/NoteList';
+
 function App() {
-  return <div className="App"></div>;
+  const [userId] = useState('11111111-1111-1111-1111-111111111111');
+
+  return (
+    <div className="container mt-4">
+      <h1>Sticky Notes</h1>
+      <NoteList userId={userId} />
+    </div>
+  );
 }
 
 export default App;
