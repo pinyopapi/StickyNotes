45e72cc64cf45377fa73a48633987dee5d7d90f5
pinyopapi <95istvan@gmail.com>
2025-10-26 07:45:09 +0100

    Add create endpoint test

---
+++ src/StickyNotes.Tests/StickyNotes.Tests/ApiTests/NoteControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using StickyNotes.Api.Controllers;
+
+namespace StickyNotes.Tests.ApiTests
+{
+
+    [TestFixture]
+    public class NotesControllerTests
+    {
+        private NotesController _controller;
+        private FakeNoteService _service;
+        private Guid _userId;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new FakeNoteService();
+            _controller = new NotesController(_service);
+            _userId = Guid.NewGuid();
+        }
+
+        [Test]
+        public async Task Create_ShouldReturnCreatedNote()
+        {
+            var request = new CreateNoteRequest("Title", "Content", _userId);
+            var result = await _controller.Create(request);
+            Assert.That(result, Is.Not.Null);
+        }
+
+    }
+}
