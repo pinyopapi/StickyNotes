e56b1dabba8bdabe5e03d64e66db994978f42910
pinyopapi <95istvan@gmail.com>
2025-10-26 11:29:40 +0100

    Expand note list with onUpdate

--- frontend/src/components/NoteList.jsx
+++ frontend/src/components/NoteList.jsx
@@ -5,13 +5,19 @@ import NoteCard from './NoteCard';
 const NoteList = ({ userId }) => {
     const [notes, setNotes] = useState([]);
 
-    useEffect(() => {
+    const fetchNotes = () => {
         getAllNotes(userId).then(res => setNotes(res.data));
+    };
+
+    useEffect(() => {
+        fetchNotes();
     }, [userId]);
 
     return (
         <div className="d-flex flex-wrap">
-            {notes.map(note => <NoteCard key={note.id} note={note} />)}
+            {notes.map(note => (
+                <NoteCard key={note.id} note={note} onUpdate={fetchNotes} />
+            ))}
         </div>
     );
 };
