a06c83c877cde40e0fe31553489c82c82f10ca35
pinyopapi <95istvan@gmail.com>
2025-11-19 14:35:48 +0100

    Add env url

--- frontend/src/services/noteService.js
+++ frontend/src/services/noteService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/note';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getAllNotes = (userId) => {
     return axios.get(API_URL, { params: { userId } });
